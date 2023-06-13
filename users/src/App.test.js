abfa3cda13431c27b9dfd601231b5f01f3accac9
bobrock8 <aleksandarilic990@gmail.com>
2023-06-13 14:34:18 +0200

    test whole app

--- users/src/App.test.js
+++ users/src/App.test.js
@@ -1,8 +1,33 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, within } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
+test('can receive a new user and show it on a list', () => {
+
+  const testName = 'Miskonjica';
+  const testEmail = 'kumce@kumce.com';
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const nameInput = screen.getByLabelText(/enter name/i);
+  const emailInput = screen.getByLabelText(/please enter email/i);
+  const submit = screen.getByRole('button');
+
+  user.click(nameInput);
+  user.keyboard(testName);
+
+  user.click(emailInput);
+  user.keyboard(testEmail);
+
+  user.click(submit);
+  // screen.debug();
+
+  const rows = within(screen.getByTestId('users')).getAllByRole('row');
+  const tableName = screen.getByRole('cell', {name: testName});
+  const tableEmail = screen.getByRole('cell', {name: testEmail});
+
+  expect(rows).toHaveLength(1);
+  expect(tableName).toBeInTheDocument();
+  expect(tableEmail).toBeInTheDocument();
+
 });
