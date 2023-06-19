3dfd3767988a99820255b2699979e75d0d4c628c
bobrock8 <aleksandarilic990@gmail.com>
2023-06-19 12:12:43 +0200

    add icon to the component and finish test for that

--- codesplain/src/components/repositories/RepositoriesListItem.test.js
+++ codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -13,16 +13,36 @@ function renderComponent() {
         html_url: 'https://github.com/facebook/react'
     }
 
-    return render(
+    render(
         <MemoryRouter>
             <RepositoriesListItem repository={repository} />
         </MemoryRouter>
     
     );
+
+    return { repository };
 }
 
 test('show a link to the github page for this repository', async ()=>{
-    renderComponent();
+    const { repository } = renderComponent();
+
+    // wait for FileIcon component (there is async process)
     await screen.findByRole('img', { name: 'Javascript' });
+
+    const link = screen.getByRole('link', {
+        name: /github repository/i
+    });
+
+    expect(link).toHaveAttribute('href', repository.html_url);
+
+});
+
+test('show a fileicon with appropriate icon', async ()=>{
+    const { repository } = renderComponent();
+
+    // wait for FileIcon component (there is async process)
+    const fileIcon = await screen.findByRole('img', { name: 'Javascript' });
+
+    expect(fileIcon).toHaveClass('js-icon');
 
 });
