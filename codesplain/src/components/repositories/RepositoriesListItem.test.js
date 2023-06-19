be85d23adfe1590a6f79e3fbaae50791b88c029b
bobrock8 <aleksandarilic990@gmail.com>
2023-06-19 11:38:23 +0200

    fix fileicon act warning 1st way

---
+++ codesplain/src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,36 @@
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import RepositoriesListItem from './RepositoriesListItem';
+
+
+function renderComponent() {
+    const repository = {
+        full_name: 'facebook/react',
+        language: 'Javascript',
+        description: 'JS librar',
+        owner: 'facebook',
+        name: 'react',
+        html_url: 'https://github.com/facebook/react'
+    }
+
+    return render(
+        <MemoryRouter>
+            <RepositoriesListItem repository={repository} />
+        </MemoryRouter>
+    
+    );
+}
+
+test('show a link to the github page for this repository', async ()=>{
+    renderComponent();
+
+    await screen.findByRole('img', { name: 'Javascript' });
+});
+
+const pause = () => {
+    return new Promise(resolve => {
+        setTimeout(()=> {
+            resolve();
+        }, 1000);
+    })
+}
