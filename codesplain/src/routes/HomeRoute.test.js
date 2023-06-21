a5e4ed046c21a0d5643f6ccf53cd73b896933235
bobrock8 <aleksandarilic990@gmail.com>
2023-06-21 12:22:49 +0200

    make msw server dinamically

--- codesplain/src/routes/HomeRoute.test.js
+++ codesplain/src/routes/HomeRoute.test.js
@@ -3,39 +3,25 @@ import { setupServer } from 'msw/node';
 import { rest } from 'msw';
 import { MemoryRouter } from 'react-router-dom';
 import HomeRoute from './HomeRoute';
+import { creteServer } from '../test/server';
 
 
-const handlers = [
-    rest.get('/api/repositories', (req, res, ctx) => {
-        const language = req.url.searchParams.get('q').split('language:')[1];
-        console.log(language);
-
-        return res(
-            ctx.json({
+creteServer([
+    {
+        path: '/api/repositories',
+        method: 'get',
+        res: (req, res, ctx) => {
+            const language = req.url.searchParams.get('q').split('language:')[1];
+            return {
                 items:[
                     { id: 1, full_name: `${language}_one` },
                     { id: 2, full_name: `${language}_two` },
                 ]
-            })
-        )
+            }
+        }
+    }
+]);
 
-    }),
-];
-
-
-const server = setupServer(...handlers);
-
-beforeAll(() => {
-    server.listen();
-});
-
-afterEach(() => {
-    server.resetHandlers();
-});
-
-afterAll(() => {
-    server.close();
-});
 
 test('renders two links for each language', async () => {
     render(
