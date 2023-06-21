55397d55d17385fae6d23263461f7a88c3ae2f96
bobrock8 <aleksandarilic990@gmail.com>
2023-06-21 13:02:14 +0200

    understand how to test auth components

--- codesplain/src/components/auth/AuthButtons.test.js
+++ codesplain/src/components/auth/AuthButtons.test.js
@@ -3,21 +3,18 @@ import { MemoryRouter } from 'react-router';
 import { createServer } from '../../test/server';
 import AuthButtons from './AuthButtons';
 
-
+// createServer() --> GET '/api/user' --> { user: null }
 test('when user is not signed in, sign in and sign up are visible', async() => {
 
 });
-
-
 test('when user is not signed in, sign out is not visible', async() => {
 
 });
 
-
+// createServer() --> GET '/api/user' --> { user: { id: 3, email: 'ac@Wsad.com' } }
 test('when user is signed in, sign in and sign up are not visible', async() => {
 
 });
-
 test('when user is signed in, sign out is visible', async() => {
 
 });
